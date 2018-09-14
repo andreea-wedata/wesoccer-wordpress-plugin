9090c5d5cf53eccb1f4dc26f35f0ec4af3eca313
Andreea Stoenescu <andreea@wedata.co.uk>
2018-09-14 13:45:46 +0100

    add the correct path for the css file

--- wesoccer.php
+++ wesoccer.php
@@ -49,7 +49,7 @@ include( dirname(__FILE__) . '/shortcode-fixture-display.php' );
     
     function wesoccer_enqueue_page_template_styles() {
         if ( is_page( 'wesoccer-competition' ) ) {
-            wp_enqueue_style( 'page-template', 'assets/wesoccer.css' );
+            wp_enqueue_style( 'page-template', plugins_url('/assets/wesoccer.css',__FILE__ ));
         }
     }
     add_action( 'wp_enqueue_scripts', 'wesoccer_enqueue_page_template_styles' );
